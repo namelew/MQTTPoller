5b9b3798c7e9966cc5cba2bcec53609a01e0cb62
“namelew” <diogomaciel.cunha@gmail.com>
2023-02-17 17:34:52 -0300

    creating communication package

---
+++ communication/maintenance.go
@@ -0,0 +1,449 @@
+package communication
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"math/rand"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/namelew/mqtt-bm-latency/history"
+	"github.com/namelew/mqtt-bm-latency/messages"
+	"github.com/namelew/mqtt-bm-latency/utils"
+)
+
+var logMutex sync.Mutex
+var experimentListMutex sync.Mutex
+var experimentList history.OngoingExperiments
+
+func loadArguments(file string, arg map[string]interface{}) (bool, int64){
+	var arguments messages.CommandExperiment
+	jsonObj,_ := json.Marshal(arg)
+	json.Unmarshal(jsonObj, &arguments)
+
+	f, _ := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    defer f.Close()
+
+	var argf string = ""
+
+	isNull := func (param string) string{if param == ""{ return "#"}; return ""}
+
+	argf += fmt.Sprintf("broker=%s\n", arguments.Broker)
+	argf += fmt.Sprintf("broker_port=%d\n", arguments.Port)
+	argf += fmt.Sprintf("mqtt_version=%d\n", arguments.MqttVersion)
+	argf += fmt.Sprintf("num_publishers=%d\n", arguments.NumPublishers)
+	argf += fmt.Sprintf("num_subscribers=%d\n", arguments.NumSubscriber)
+	argf += fmt.Sprintf("qos_publisher=%d\n", arguments.QosPublisher)
+	argf += fmt.Sprintf("qos_subscriber=%d\n", arguments.QosSubscriber)
+	argf += fmt.Sprintf("shared_subscription=%t\n", arguments.SharedSubscrition)
+	argf += fmt.Sprintf("retain=%t\n", arguments.Retain)
+	argf += fmt.Sprintf("topic=%s\n", arguments.Topic)
+	argf += fmt.Sprintf("payload=%d\n", arguments.Payload)
+	argf += fmt.Sprintf("num_messages=%d\n", arguments.NumMessages)
+	argf += fmt.Sprintf("ramp_up=%d\n", arguments.RampUp)
+	argf += fmt.Sprintf("ramp_down=%d\n", arguments.RampDown)
+	argf += fmt.Sprintf("interval=%d\n", arguments.Interval)
+	argf += fmt.Sprintf("subscriber_timeout=%d\n", arguments.SubscriberTimeout)
+	argf += fmt.Sprintf("log_level=%s\n", arguments.LogLevel)
+	argf += fmt.Sprintf("%sntp=%s\n", isNull(arguments.Ntp),arguments.Ntp)
+	if arguments.Output {argf += fmt.Sprintf("output=%s\n", "output")}
+	argf += fmt.Sprintf("%suser_name=%s\n", isNull(arguments.User),arguments.User)
+	argf += fmt.Sprintf("%spassword=%s\n", isNull(arguments.Password),arguments.Password)
+	argf += fmt.Sprintf("%stls_truststore=%s\n", isNull(arguments.TlsTrustsore), arguments.TlsTrustsore)
+	argf += fmt.Sprintf("%stls_truststore_pass=%s\n", isNull(arguments.TlsTruststorePassword), arguments.TlsTruststorePassword)
+	argf += fmt.Sprintf("%stls_keystore=%s\n", isNull(arguments.TlsKeystore),arguments.TlsKeystore)
+	argf += fmt.Sprintf("%stls_keystore_pass=%s\n", isNull(arguments.TlsKeystorePassword),arguments.TlsKeystorePassword)
+
+    // Write bytes to file
+    byteSlice := []byte(argf)
+    _, err := f.Write(byteSlice)
+    if err != nil {
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("crash "+err.Error()+"\n")
+		logMutex.Unlock()
+		f.Close()
+    }
+
+	return arguments.Output,arguments.Expid
+}
+
+func extracExperimentResults(output string, createLog bool) messages.ExperimentResult{
+	results:= messages.ExperimentResult{}
+	results.Meta.Literal = output
+
+	temp := [12]string{}
+
+	i := 0
+
+	for _,s := range strings.Split(output, "\n"){
+		if s != "" && s[0] != '-'{
+			temp[i] = strings.Split(s, ": ")[1]
+			i++
+		}
+	}
+	results.Meta.ToolName = "mqttLoader"
+	results.Meta.ExperimentError = ""
+	
+	results.Publish.Throughput,_ = strconv.ParseFloat(strings.Replace(temp[2], ",",".", 1), 64) 
+	results.Publish.AvgThroughput,_ = strconv.ParseFloat(strings.Replace(temp[3], ",",".", 1), 64)
+	results.Publish.PubMessages,_ = strconv.Atoi(temp[4])
+
+	for _,s := range strings.Split(temp[5], ", "){
+		aux,_ := strconv.Atoi(s)
+		results.Publish.PerSecondThrouput = append(results.Publish.PerSecondThrouput, aux) 
+	}
+
+	results.Subscribe.Throughput,_ = strconv.ParseFloat(strings.Replace(temp[6], ",",".", 1), 64)
+	results.Subscribe.AvgThroughput,_ = strconv.ParseFloat(strings.Replace(temp[7], ",",".", 1), 64)
+	results.Subscribe.ReceivedMessages,_ = strconv.Atoi(temp[8])
+
+	for _,s := range strings.Split(temp[9], ", "){
+		aux,_ := strconv.Atoi(s)
+		results.Subscribe.PerSecondThrouput = append(results.Subscribe.PerSecondThrouput, aux) 
+	}
+
+	results.Subscribe.Latency,_ = strconv.ParseFloat(strings.Replace(temp[10], ",",".", 1), 64)
+	results.Subscribe.AvgLatency,_ = strconv.ParseFloat(strings.Replace(temp[11], ",",".", 1), 64)
+
+	if createLog {
+		var files []string
+
+		err := filepath.Walk("output", func(path string, info os.FileInfo, err error) error {
+			if err != nil{
+				logMutex.Lock()
+				f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				f.WriteString("extract failure "+err.Error()+"\n")
+				f.Close()
+				logMutex.Unlock()
+				return nil
+			}
+
+			if !info.IsDir(){
+				files = append(files, path)
+			}
+
+			return nil
+		})
+
+		if err != nil{
+			results.Meta.ExperimentError = "Error X: Failed in upload log file"
+		}
+
+		for _,f := range files{
+			aux := strings.Split(f, "/")
+			name := aux[len(aux) - 1]
+			if name[0:10] == "mqttloader"{
+				buffer,_ := ioutil.ReadFile(f)
+				results.Meta.LogFile.Data = buffer
+				results.Meta.LogFile.Name = name
+				results.Meta.LogFile.Extension = strings.Split(name, ".")[1]
+				os.Remove(f)
+			}
+		}
+	}
+
+	return results
+}
+
+func workerKeepAlive(client mqtt.Client, msg string){
+	for {
+		Ping(client, msg)
+		time.Sleep(time.Second)
+	}
+}
+
+func getFailExperiements() []string{
+	var temp []byte
+	var experiments []string
+	logMutex.Lock()
+	f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	f.Read(temp)
+
+	logData := strings.Split(string(temp), "\n")
+
+	f.Close()
+	logMutex.Unlock()
+
+	for i := 0; i < len(logData); i++{
+		line := strings.Split(logData[i], " ")
+		if line[0] == "start"{
+			finished := false
+			for j := i; j < len(logData); j++{
+				line2 := strings.Split(logData[j], " ")
+				if (line2[0] == "finish" || line2[0] == "error") && line[2] == line2[2]{
+					finished = true
+					break
+				}
+			}
+			if !finished{
+				experiments = append(experiments, line[2])
+			}
+		}
+	}
+
+	return experiments
+}
+
+func redo(client mqtt.Client, clientID string, tool string, redoList []string){
+	for _,file := range redoList{
+		msg := utils.GetJsonFromFile("CommandsLog/experiment_"+file+".json")
+		if msg != ""{
+			var cmd messages.Command
+			err := json.Unmarshal([]byte(msg), &cmd)
+			if err != nil{
+				mess,_ := json.Marshal(messages.Status{Type: "Client Status", Status: "offline " + err.Error(), Attr: messages.Command{}})
+				t := client.Publish(clientID+"/Status", byte(1), true, string(mess))
+				t.Wait()
+				logMutex.Lock()
+				f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				f.WriteString("crash "+err.Error()+"\n")
+				f.Close()
+				logMutex.Unlock()
+				os.Exit(3)
+			}
+			id,_ := strconv.ParseInt(file, 10, 64)
+			go Start(client, clientID, tool, cmd, msg, id)
+		}		
+	}
+}
+
+func Init(broker string, tool string,loginTimeout int, isUnix bool) {
+	var clientID string = "Client_"
+	var seed rand.Source
+	var random *rand.Rand
+	var makeRegister bool = false
+	var login_confirmation bool = false
+	var register_confirmation bool = false
+	var currentSession messages.Session
+
+	currentSession.Finish = true
+
+	if !utils.FileExists("worker.log"){
+		f,_ := os.Create("worker.log")
+		f.Close()
+	} else{
+		os.Truncate("worker.log", 0)
+	}
+
+	if !utils.FileExists("token.bin"){
+		for i := 0; i < 10; i++{
+			seed = rand.NewSource(time.Now().UnixNano())
+			random = rand.New(seed)
+			clientID += fmt.Sprintf("%d", random.Int() % 10)
+		}
+		makeRegister = true
+		login_confirmation = true
+	} else{
+		data,_ := os.ReadFile("token.bin")
+		clientID = strings.Split(string(data), "\n")[0]
+		register_confirmation = true
+	}
+
+	ka, _ := time.ParseDuration(strconv.Itoa(10000) + "s")
+
+	opts := mqtt.NewClientOptions().
+			AddBroker(broker).
+			SetClientID(clientID).
+			SetCleanSession(true).
+			SetAutoReconnect(true).
+			SetKeepAlive(ka).
+			SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {}).
+			SetConnectionLostHandler(func(client mqtt.Client, reason error) {})
+	
+	client := mqtt.NewClient(opts)
+
+	tokenConnection := client.Connect()
+
+	tokenConnection.Wait()
+
+	token := client.Subscribe(clientID+"/Login/Log", byte(1), func(c mqtt.Client, m mqtt.Message) {
+		login_confirmation = true
+	})
+	token.Wait()
+
+	token = client.Subscribe("Orquestrator/Register/Log", byte(1), func(c mqtt.Client, m mqtt.Message) {
+		response := strings.Split(string(m.Payload()), "-")
+
+		if response[0] != clientID {
+			mess,_ := json.Marshal(messages.Status{Type:"Client Status", Status: "offline registration fail", Attr: messages.Command{}})
+			token = client.Publish(clientID+"/Status", byte(1), true, string(mess))
+			token.Wait()
+			client.Disconnect(0)
+			logMutex.Lock()
+			f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			f.WriteString("shutdown register failure\n")
+			f.Close()
+			logMutex.Unlock()
+			os.Exit(0)
+			return
+		}
+
+		var f *os.File
+
+		if!utils.FileExists("token.bin"){
+			f,_ = os.Create("token.bin")
+		} else{
+			f,_ = os.Open("token.bin")
+		}
+		f.Truncate(0)
+		f.Write([]byte(response[1]))
+
+		f.Close()
+		register_confirmation = true
+	})
+	token.Wait()
+
+	if makeRegister {
+		token = client.Publish("Orquestrator/Register", byte(1), false, clientID)
+		token.Wait()
+
+		cd := 0
+		for !register_confirmation{
+			if cd >= loginTimeout{
+				break
+			}
+			time.Sleep(time.Second)
+			cd++
+		}
+	}else{
+		token = client.Publish("Orquestrator/Login", byte(1), false, clientID)
+		token.Wait()
+
+		cd := 0
+		for !login_confirmation{
+			if cd >= loginTimeout{
+				break
+			}
+			time.Sleep(time.Second)
+			cd++
+		}
+	}
+
+	if !login_confirmation{
+		mess,_ := json.Marshal(messages.Status{Type: "Client Status", Status: "offline login fail",Attr: messages.Command{}})
+		token = client.Publish(clientID+"/Status", byte(1), true, string(mess))
+		token.Wait()
+		client.Disconnect(0)
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("shutdown login failure\n")
+		f.Close()
+		logMutex.Unlock()
+		os.Exit(0)
+	}
+
+	if !register_confirmation{
+		mess,_ := json.Marshal(messages.Status{Type: "Client Status", Status: "offline registration fail", Attr: messages.Command{}})
+		token = client.Publish(clientID+"/Status", byte(1), true, string(mess))
+		token.Wait()
+		client.Disconnect(0)
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("shutdown register failure\n")
+		f.Close()
+		logMutex.Unlock()
+		os.Exit(0)
+	}
+
+	mess,_ := json.Marshal(messages.Status{Type: "Client Status", Status: "online", Attr: messages.Command{}})
+	token = client.Publish(clientID+"/Status", byte(1), true, string(mess))
+	token.Wait()
+
+	token = client.Subscribe(clientID+"/Command", byte(1), func(c mqtt.Client, m mqtt.Message) {
+		if m.Retained(){
+			return
+		}
+		message := m.Payload()
+
+		var commd messages.Command
+		err := json.Unmarshal(message, &commd)
+		if err != nil {
+			mess,_ = json.Marshal(messages.Status{Type: "Client Status", Status: "offline " + err.Error(), Attr: messages.Command{}})
+			t := client.Publish(clientID+"/Status", byte(1), true, string(mess))
+			t.Wait()
+			logMutex.Lock()
+			f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			f.WriteString("crash "+err.Error()+"\n")
+			f.Close()
+			logMutex.Unlock()
+			os.Exit(3)
+		}
+
+		switch commd.Name{
+			case "info":
+				var arguments messages.Info
+				jsonObj,_ := json.Marshal(commd.Arguments)
+				json.Unmarshal(jsonObj, &arguments)
+
+				go Info(client, arguments, isUnix, clientID)
+			case "start":
+				go Start(client, clientID, tool, commd, string(m.Payload()), -1)
+			case "cancel":
+				experimentListMutex.Lock()
+				node := experimentList.Search(int64(commd.Arguments["id"].(float64)))
+				if node != nil{
+					node.Finished = true
+					node.Proc.Kill()
+				}
+				experimentListMutex.Unlock()
+		}
+	})
+
+	token.Wait()
+
+	token = client.Subscribe(clientID+"/Ping", byte(1), func(c mqtt.Client, m mqtt.Message) {
+		Ping(client, clientID)
+	})
+
+	token.Wait()
+
+	token = client.Subscribe("Orquestrator/Sessions", byte(1), func(c mqtt.Client, m mqtt.Message) {
+		json.Unmarshal(m.Payload(), &currentSession.Status)
+		temp := strings.Split(currentSession.Status.Status, " ") 
+		if temp[0] == "start"{
+			currentSession.Id,_ = strconv.Atoi(temp[2])
+			currentSession.Finish = false
+		}else{
+			currentSession.Finish = true
+			os.RemoveAll("CommandsLog")
+		}
+	})
+	token.Wait()
+
+	go workerKeepAlive(client, clientID)
+
+	if !currentSession.Finish{
+		redoList := getFailExperiements()
+		redo(client, clientID, tool, redoList)
+	}
+
+	c := make(chan os.Signal)
+
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<- c
+		mess,_ := json.Marshal(messages.Status{Type: "Client messages.Status", Status: "offline", Attr: messages.Command{}})
+		token := client.Publish(clientID+"/Status", byte(1), true, string(mess))
+		token.Wait()
+		client.Disconnect(0)
+
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("shutdown\n")
+		f.Close()
+		logMutex.Unlock()
+		os.Exit(1)
+	}()
+}
---
+++ communication/publish.go
@@ -0,0 +1,169 @@
+package communication
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/namelew/mqtt-bm-latency/history"
+	"github.com/namelew/mqtt-bm-latency/messages"
+	"github.com/namelew/mqtt-bm-latency/utils"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func Ping(client mqtt.Client, m string){
+	t := client.Publish("Orquestrator/Ping", byte(1), false, m)
+	t.Wait()
+}
+
+func Start(client mqtt.Client, clientID string, tool string, cmdExp messages.Command, commandLiteral string, experimentId int64){
+	var arg_file string = `"myconfig.conf"`
+	var flag string = "-c"
+	var createLogFile bool = false
+	var id int64
+
+	if(!utils.FileExists("myconfig.conf")){
+		file,_ := os.Create("myconfig.conf")
+		file.Close()
+	}
+	os.Truncate("myconfig.conf", 0)
+
+	if cmdExp.Arguments != nil{
+		createLogFile,id = loadArguments("myconfig.conf", cmdExp.Arguments)
+	} else{
+		arg_file = ""
+		flag = ""
+	}
+
+	if experimentId != -1{
+		id = experimentId
+	}
+
+	if !utils.FileExists("CommandsLog/experiment_"+fmt.Sprint(id)+".json"){
+		file,_ :=os.Create("CommandsLog/experiment_"+fmt.Sprint(id)+".json")
+		file.Write([]byte(commandLiteral))
+		file.Close()
+	}
+
+	logMutex.Lock()
+	f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f.WriteString("start experiment "+ strconv.FormatInt(id, 10) + "\n")
+	f.Close()
+	logMutex.Unlock()
+
+	cmd := exec.Command("./"+tool, flag ,arg_file)
+
+	mess,_ := json.Marshal(messages.Status{Type: fmt.Sprintf("Experiment Status %d", id), Status: "start",Attr:  cmdExp}) 
+	t := client.Publish(clientID+"/Experiments/Status", byte(1), true, string(mess))
+	t.Wait()
+	var output bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &output
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+
+	experimentNode := history.CreateRegister(id, cmd.Process)
+
+	experimentListMutex.Lock()
+	experimentList.Add(&experimentNode)
+	experimentListMutex.Unlock()
+
+	cmd.Wait()
+
+	experimentListMutex.Lock()
+	if experimentNode.Finished{
+		experimentList.Remove(id)
+		experimentListMutex.Unlock()
+		return
+	}
+	experimentList.Remove(id)
+	experimentListMutex.Unlock()
+	
+	if err != nil{
+		mess,_ = json.Marshal(messages.Status{Type: fmt.Sprintf("Experiment Status %d", id) , Status: fmt.Sprint(err) + ": " + stderr.String(), Attr: messages.Command{}})
+		t = client.Publish(clientID+"/Experiments/Status", byte(1), true, string(mess))
+		t.Wait()
+
+		mess,_ = json.Marshal(messages.Status{Type: "Client Status", Status: "offline " + err.Error(), Attr: messages.Command{}})
+		t = client.Publish(clientID+"/Status", byte(1), true, string(mess))
+		t.Wait()
+
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("crash experiment "+strconv.FormatInt(id, 10)+" error "+err.Error()+"\n")
+		f.Close()
+		logMutex.Unlock()
+		os.Exit(3)
+	}
+
+	resultsExperiment := extracExperimentResults(output.String(), createLogFile)
+
+	if resultsExperiment.Publish.AvgThroughput == 0{
+		mess,_ = json.Marshal(messages.Status{Type: fmt.Sprintf("Experiment Status %d", id), Status: "Error 10: Hardware Colapse", Attr: messages.Command{}})
+		t = client.Publish(clientID+"/Experiments/Status", byte(1), true, string(mess))
+		t.Wait()
+
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("error experiment "+strconv.FormatInt(id, 10)+" hardware colapse\n")
+		f.Close()
+		logMutex.Unlock()
+	} else {
+		mess,_ = json.Marshal(messages.Status{Type: fmt.Sprintf("Experiment Status %d", id), Status: "finish", Attr: messages.Command{}})
+		t = client.Publish(clientID+"/Experiments/Status", byte(1), true, string(mess))
+		t.Wait()
+
+		logMutex.Lock()
+		f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f.WriteString("finish experiment "+strconv.FormatInt(id, 10)+"\n")
+		f.Close()
+		logMutex.Unlock()
+	}
+
+	resultsExperiment.Meta.ID = uint64(id)
+
+	results,_ := json.Marshal(resultsExperiment)
+
+	t = client.Publish(clientID+"/Experiments/Results", byte(1), false, string(results))
+	t.Wait()
+
+	os.Remove("CommandsLog/experiment_"+fmt.Sprint(id)+".json")
+}
+
+func Info(client mqtt.Client, arguments messages.Info, isUnix bool, clientID string){
+	var result messages.InfoDisplay
+
+	logMutex.Lock()
+	f,_ := os.OpenFile("worker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f.WriteString("execute info\n")
+	f.Close()
+	logMutex.Unlock()
+	
+	if arguments.DiscDisplay{
+		rootPath := "/"
+		if !isUnix{
+			rootPath = "\\"
+		}
+		diskStat,_ := disk.Usage(rootPath)
+		result.Disk = diskStat.Total/1024/1024
+	}
+	if arguments.MemoryDisplay{
+		vmStat, _ := mem.VirtualMemory()
+		result.Ram = vmStat.Total/1024/1024
+	}
+	if arguments.CpuDisplay{
+		cpuStat, _ := cpu.Info()
+		result.Cpu = cpuStat[0].ModelName
+	}
+
+	resp,_ := json.Marshal(result)
+
+	t := client.Publish(clientID + "/Info", byte(1), false, string(resp))
+	t.Wait()
+}
