edf2f6ac61463f3f1ecf866fff7264413c38ddf2
namelew <diogomaciel.cunha@gmail.com>
2023-09-05 11:26:22 -0300

    Adding load data from data files

--- core/internal/orquestrator/data/services/services.go
+++ core/internal/orquestrator/data/services/services.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/namelew/mqtt-bm-latency/internal/orquestrator/data/models"
 	"github.com/namelew/mqtt-bm-latency/packages/logs"
+	"github.com/namelew/mqtt-bm-latency/packages/utils"
 	"github.com/tidwall/btree"
 	"golang.org/x/exp/constraints"
 )
@@ -32,6 +33,10 @@ func Build[I constraints.Ordered, K TableData](filename string, l *logs.Log) *Ta
 	new := Table[I, K]{
 		log:      l,
 		filename: filename,
+	}
+
+	if utils.FileExists(filename) && utils.FileExists(filename+".index") {
+		new.loadFiles()
 	}
 
 	go new.syncer()
@@ -102,10 +107,18 @@ func (h *Table[I, K]) syncer() {
 
 		h.mutex.Lock()
 
-		f, err := os.OpenFile(h.filename, os.O_CREATE|os.O_WRONLY, 0644)
+		datafile, err := os.OpenFile(h.filename, os.O_CREATE|os.O_WRONLY, 0644)
 
 		if err != nil {
 			h.log.Register("Unable to open data file: " + h.filename + ". " + err.Error())
+			h.mutex.Unlock()
+			continue
+		}
+
+		indexfile, err := os.OpenFile(h.filename+".index", os.O_CREATE|os.O_WRONLY, 0644)
+
+		if err != nil {
+			h.log.Register("Unable to open index file: " + h.filename + ".index. " + err.Error())
 			h.mutex.Unlock()
 			continue
 		}
@@ -114,19 +127,98 @@ func (h *Table[I, K]) syncer() {
 		encoder := gob.NewEncoder(&buffer)
 
 		if err := encoder.Encode(h.data.Values()); err != nil {
+			datafile.Close()
 			h.log.Register("unable to enconde data to write in " + h.filename + ". " + err.Error())
 			h.mutex.Unlock()
 			continue
 		}
 
-		_, err = f.Write(buffer.Bytes())
+		_, err = datafile.Write(buffer.Bytes())
 
 		if err != nil {
+			datafile.Close()
 			h.log.Register("unable to write in the datafile " + h.filename + ". " + err.Error())
 			h.mutex.Unlock()
 			continue
 		}
 
+		buffer.Reset()
+
+		if err := encoder.Encode(h.data.Keys()); err != nil {
+			indexfile.Close()
+			h.log.Register("unable to enconde data to write in " + h.filename + ".index. " + err.Error())
+			h.mutex.Unlock()
+			continue
+		}
+
+		_, err = indexfile.Write(buffer.Bytes())
+
+		if err != nil {
+			h.log.Register("unable to write in the indexfile " + h.filename + ".index. " + err.Error())
+			indexfile.Close()
+			h.mutex.Unlock()
+			continue
+		}
+
 		h.mutex.Unlock()
+		datafile.Close()
+		indexfile.Close()
+	}
+}
+
+func (h *Table[I, K]) loadFiles() {
+	var buffer bytes.Buffer
+	decoder := gob.NewDecoder(&buffer)
+	keys := make([]I, 0)
+	values := make([]K, 0)
+
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	datafileBytes, err := os.ReadFile(h.filename)
+
+	if err != nil {
+		h.log.Fatal("Unable to open data file: " + h.filename + ". " + err.Error())
+	}
+
+	indexfileBytes, err := os.ReadFile(h.filename + ".index")
+
+	if err != nil {
+		h.log.Fatal("Unable to open index file: " + h.filename + "index. " + err.Error())
+	}
+
+	_, err = buffer.Write(datafileBytes)
+
+	if err != nil {
+		h.log.Fatal("Unable to store the data of the datafiel in a buffer. " + err.Error())
+	}
+
+	err = decoder.Decode(&values)
+
+	if err != nil {
+		h.log.Fatal("Unable to decode datafile data. " + err.Error())
+	}
+
+	buffer.Reset()
+
+	_, err = buffer.Write(indexfileBytes)
+
+	if err != nil {
+		h.log.Fatal("Unable to store the data of the indexfile in a buffer. " + err.Error())
+	}
+
+	err = decoder.Decode(&keys)
+
+	if err != nil {
+		h.log.Fatal("Unable to decode indexfile data. " + err.Error())
+	}
+
+	if len(keys) != len(values) {
+		h.log.Register("Inconsistent data. " + err.Error() + "\nDiscarting pre-session data")
+		return
+	}
+
+	for i := range keys {
+		h.data.Set(keys[i], values[i])
 	}
 }
