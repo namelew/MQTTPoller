17ff68e7182f079d9cb1d4560f44d841e241255d
namelew <diogomaciel.cunha@gmail.com>
2023-09-18 14:17:16 -0300

    Finishing api messages mapping

--- src/app/interfaces/IExperiment.ts
+++ src/app/interfaces/IExperiment.ts
@@ -28,16 +28,16 @@ export interface IRequest {
         tlsTruststore:  string,
         tlsTruststorePass:  string,
         tlsKeystore:  string,
-        tlsKeystorePass:  string,
+        tlsKeystorePass:  string
     }
 };
 
 export interface IResult {
     meta: {
-        id: any,
-        error: any,
-        tool: any,
-        literal: any,
+        id: number,
+        error: string,
+        tool: string,
+        literal: string,
         log_file: {
             name: string,
             data: File,
@@ -45,21 +45,51 @@ export interface IResult {
         },
     },
     publish: {
-        max_throughput: any,
-        avg_throughput: any,
-        publiqued_messages: any,
-        per_second_throungput: any,
+        max_throughput: Number,
+        avg_throughput: Number,
+        publiqued_messages: number,
+        per_second_throungput: number[]
     },
     subscribe: {
-        max_throughput: any,
-        avg_throughput: any,
-        received_messages: any,
-        per_second_throungput: any,
-        latency: any,
-        avg_latency: any,
-    },
+        max_throughput: Number,
+        avg_throughput: Number,
+        received_messages: number,
+        per_second_throungput: number[],
+        latency: Number,
+        avg_latency: Number
+    }
 };
 
 export interface IExperiment {
-
+    id: number,
+    broker: string,
+    port:  number,
+    mqttVersion:  number,
+    numPublishers:  number,
+    numSubscribers:  number,
+    qosPublisher:  number,
+    qosSubscriber:  number,
+    sharedSubscription:  boolean,
+    retain:  boolean,
+    topic:  string,
+    payload:  number,
+    numMessages:  number,
+    ramUp:  number,
+    rampDown:  number,
+    interval:  number,
+    subscriberTimeout:  number,
+    execTime:  number,
+    logLevel:  string,
+    ntp:  string,
+    output:  boolean,
+    username:  string,
+    password:  string,
+    tlsTruststore:  string,
+    tlsTruststorePass:  string,
+    tlsKeystore:  string,
+    tlsKeystorePass:  string,
+    finish:boolean,
+    error: string,
+    results: IResult[],
+    workers: string[]
 };
