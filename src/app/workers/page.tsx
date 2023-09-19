755a50a7ef0d679d3f1adbce22d21a6e0cf5f149
namelew <diogomaciel.cunha@gmail.com>
2023-09-19 17:59:51 -0300

    Finishing data fetch to workers route

--- src/app/workers/page.tsx
+++ src/app/workers/page.tsx
@@ -1,66 +1,20 @@
-'use client'
-import { Table, Thead, Tbody, Tr, Th, Td, Checkbox, Flex, Button } from "@chakra-ui/react";
-import { useState } from "react";
 import { IWorker } from "../interfaces/IWorker";
+import WorkersTable from "./table";
+import { api } from "../_consumer";
 
-const Homepage = () => {
-    const [selectAll, setSelectAll] = useState(false);
-    const [selectedWorkers, setSelectedWorkers] = useState<string[]>([]);
-    const [workers, setWorkers] = useState<IWorker[]>();
+const Homepage = async () => {
+    const response = await api.get<IWorker[]>("/worker");
+    let workers:IWorker[] | undefined = undefined;
 
-    const handleSelectAll = () => {
-        if (workers) {
-            if (selectAll) {
-                setSelectedWorkers([]);
-            } else {
-                setSelectedWorkers(workers.map(worker => worker.id));
-            }
-            setSelectAll(!selectAll);
-        }
-    };
-
-    const handleSelectRow = (id:string) => {
-        if (selectedWorkers.includes(id)) {
-            setSelectedWorkers(selectedWorkers.filter((rowId) => rowId !== id));
-        } else {
-            setSelectedWorkers([...selectedWorkers, id]);
-        }
-    };
-
-    const startExperiment = () => {
-
-    };
+    if (response.status !== 200) {
+        alert(response.statusText);
+    } else {
+        workers = response.data;
+    }
 
     return (
         <>
-            <Flex justifyContent={'flex-end'}>
-                <Button onClick={() => startExperiment()}>Iniciar Experimento</Button>
-            </Flex>
-            <Table variant="simple">
-                <Thead>
-                    <Tr>
-                    <Th>
-                        <Checkbox isChecked={selectAll} onChange={handleSelectAll} />
-                    </Th>
-                    <Th>ID</Th>
-                    <Th>Status</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {workers?.map((worker) => (
-                    <Tr key={worker.id}>
-                        <Td>
-                        <Checkbox
-                            isChecked={selectedWorkers.includes(worker.id)}
-                            onChange={() => handleSelectRow(worker.id)}
-                        />
-                        </Td>
-                        <Td>{worker.id}</Td>
-                        <Td>{worker.online ? 'Online' : 'Offline'}</Td>
-                    </Tr>
-                    ))}
-                </Tbody>
-            </Table>
+            <WorkersTable workers={workers}/>
         </>
     );
 };
---
+++ src/app/workers/table.tsx
@@ -0,0 +1,71 @@
+'use client'
+import { Table, Thead, Tbody, Tr, Th, Td, Checkbox, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { IWorker } from "../interfaces/IWorker";
+
+interface Props {
+    workers?:IWorker[],
+}
+
+const WorkersTable = ( { workers } : Props) => {
+    const [selectAll, setSelectAll] = useState(false);
+    const [selectedWorkers, setSelectedWorkers] = useState<string[]>([]);
+
+    const handleSelectAll = () => {
+        if (workers) {
+            if (selectAll) {
+                setSelectedWorkers([]);
+            } else {
+                setSelectedWorkers(workers.map(worker => worker.id));
+            }
+            setSelectAll(!selectAll);
+        }
+    };
+
+    const handleSelectRow = (id:string) => {
+        if (selectedWorkers.includes(id)) {
+            setSelectedWorkers(selectedWorkers.filter((rowId) => rowId !== id));
+        } else {
+            setSelectedWorkers([...selectedWorkers, id]);
+        }
+    };
+
+    const startExperiment = () => {
+
+    };
+
+    return (
+        <>
+            <Flex justifyContent={'flex-end'}>
+                <Button onClick={() => startExperiment()}>Iniciar Experimento</Button>
+            </Flex>
+            <Table variant="simple">
+                <Thead>
+                    <Tr>
+                    <Th>
+                        <Checkbox isChecked={selectAll} onChange={handleSelectAll} />
+                    </Th>
+                    <Th>ID</Th>
+                    <Th>Status</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {workers?.map((worker) => (
+                    <Tr key={worker.id}>
+                        <Td>
+                        <Checkbox
+                            isChecked={selectedWorkers.includes(worker.id)}
+                            onChange={() => handleSelectRow(worker.id)}
+                        />
+                        </Td>
+                        <Td>{worker.id}</Td>
+                        <Td>{worker.online ? 'Online' : 'Offline'}</Td>
+                    </Tr>
+                    ))}
+                </Tbody>
+            </Table>
+        </>
+    );
+}
+
+export default WorkersTable;
